ee3312c9b83f9978dd4099b54486bbc5868fbb6a
Carl <47005123+creinemann@users.noreply.github.com>
2026-02-07 10:06:34 -0600

    Update layerRegistry.js
    corrected OWMClouds.js to useOWMClouds.js

--- src/plugins/layerRegistry.js
+++ src/plugins/layerRegistry.js
@@ -4,7 +4,7 @@
 
 import * as N3FJPLoggedQSOsPlugin from './layers/useN3FJPLoggedQSOs.js';
 import * as WXRadarPlugin from './layers/useWXRadar.js';
-import * as OWMClouds from './layers/OWMClouds.js';
+import * as OWMClouds from './layers/useOWMClouds.js';
 import * as EarthquakesPlugin from './layers/useEarthquakes.js';
 import * as AuroraPlugin from './layers/useAurora.js';
 import * as WSPRPlugin from './layers/useWSPR.js';
